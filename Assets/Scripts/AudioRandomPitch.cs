889e67fec6cf0a73db370bccebb938fba2cb11f4
Arttu Aalto <arttu.aalto@nextgames.com>
2016-01-31 13:45:49 +0200

    Explosion say boom

---
+++ Assets/Scripts/AudioRandomPitch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioRandomPitch : MonoBehaviour {
+
+	[SerializeField]
+	private float minPitch = 0.8f;
+
+	[SerializeField]
+	private float maxPitch = 1.2f;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		AudioSource source = GetComponent<AudioSource>();
+
+		if (source != null)
+		{
+			source.pitch = Random.Range (minPitch, maxPitch);
+		}
+	}
+}
